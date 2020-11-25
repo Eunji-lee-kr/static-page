4ee24a57cbefec18b0a24f23ac7e80445b29979a
eunji-lee-dev <eunji.lee.dev@gmail.com>
2020-11-25 22:13:25 +0100

    Add: media queries-md

--- js/main.js
+++ js/main.js
@@ -7,9 +7,23 @@
 	$toggleBtn.addEventListener('click',function () {
 		toggleElements();
 	});
+
+	window.addEventListener('resize', function () {
+		if (window.innerWidth > 1024) {
+			offElements();
+		}
+	});
+
 	function toggleElements() {
 		[].forEach.call($toggles, function (toggle) {
 			toggle.classList.toggle('on');
-		})
+		});
 	}
+
+	function offElements() {
+		[].forEach.call($toggles, function (toggle) {
+			toggle.classList.remove('on');
+		});
+	}
+
 })(window, document)
